5fe3223f4ccff82cec9427cdbb648fd9c4cdb101
Makarand Vilas <75856105+makmali@users.noreply.github.com>
2021-02-15 13:16:01 +0530

    Add files via upload

---
+++ select.js
@@ -0,0 +1,21 @@
+var headOne=document.querySelector("#one");
+
+var headTwo=document.querySelector("#two");
+
+var headThree=document.querySelector("#three");
+
+var headFour=document.querySelector("#four");
+
+
+var headFive=document.querySelector("#five");
+
+
+var headSix=document.querySelector("#six");
+
+
+console.log(headOne);
+console.log(headTwo);
+console.log(headThree);
+console.log(headFour);
+console.log(headFive);
+console.log(headSix);
